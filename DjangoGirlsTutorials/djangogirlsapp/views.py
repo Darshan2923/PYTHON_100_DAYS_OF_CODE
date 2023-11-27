649dd45efa562104a8f242b67bb6fe54dc06c1e3
Darshan <darshanpatel2923@gmail.com>
2023-11-27 21:26:49 +0530

    Learnt about django templates and dynamic changes

--- DjangoGirlsTutorials/djangogirlsapp/views.py
+++ DjangoGirlsTutorials/djangogirlsapp/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 
+# To display our QuerySet on our blog's post list, we have two things left to do:
+
+# Pass the posts QuerySet to the template context, by changing the render function call. We'll do this now.
+# Modify the template to display the posts QuerySet. We'll cover this in a later chapter.
+
 def index(request):
-    context={}
+    posts=Post.objects.filter(publish_date__lte=timezone.now()).order_by('publish_date')
+    context={'posts': posts}
     return render(request,'djangogirlsapp/index.html',context)
