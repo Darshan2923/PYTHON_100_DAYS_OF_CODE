3e5a9c9caaccf8f57360fbcee6735811838bc23e
Darshan <darshanpatel2923@gmail.com>
2023-11-30 22:27:07 +0530

    Added little to the learnings of the views

--- Django_Polls/pollsapp/urls.py
+++ Django_Polls/pollsapp/urls.py
@@ -1,7 +1,15 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('',views.index,name='index'),
+    # ex: /polls/
+    path('', views.index, name='index'),
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
+
+# Take a look in your browser, at “/polls/34/”. It’ll run the detail() method and display whatever ID you provide in the URL. Try “/polls/34/results/” and “/polls/34/vote/” too – these will display the placeholder results and voting pages.
