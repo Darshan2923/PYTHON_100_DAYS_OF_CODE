80cebca1ffd88ff6e624bbcb6b7bbd1b9e5c9e09
Darshan <darshanpatel2923@gmail.com>
2023-12-02 17:42:59 +0530

    Learnt about views and url dispatching

--- Django_Polls/pollsapp/views.py
+++ Django_Polls/pollsapp/views.py
@@ -1,16 +1,37 @@
 from django.shortcuts import render,HttpResponse
-
+from django.template import loader
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 # Create your views here.
 from .models import Question
 
 
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     template = loader.get_template('pollsapp/index.html')
+#     context = {
+#         'latest_question_list': latest_question_list,
+#     }
+#     return HttpResponse(template.render(context, request))
+
+
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'pollsapp/index.html', context)
+
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(request, 'pollsapp/detail.html', {'question': question})
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+# There’s also a get_list_or_404() function, which works just as get_object_or_404() – except using filter() instead of get(). It raises Http404 if the list is empty.
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
