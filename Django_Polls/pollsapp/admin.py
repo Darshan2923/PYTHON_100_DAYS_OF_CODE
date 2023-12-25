45e14b8780852b8a04ac0ad97e8d52a09ced88eb
Darshan <darshanpatel2923@gmail.com>
2023-12-25 19:10:19 +0530

    Customizing admin panel

--- Django_Polls/pollsapp/admin.py
+++ Django_Polls/pollsapp/admin.py
@@ -1,6 +1,41 @@
 from django.contrib import admin
 from .models import *
 
+# class QuestionAdmin(admin.ModelAdmin):
+#     fields=['pub_date','question_text']
+
+# class QuestionAdmin(admin.ModelAdmin):
+#     fieldsets = [
+#         (None,               {'fields': ['question_text']}),
+#         ('Date information', {'fields': ['pub_date']}),
+#     ]
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+
+
+
+
+
 # Register your models here.
-admin.site.register(Question)
-admin.site.register(Choice)
+admin.site.register(Question,QuestionAdmin)
+# admin.site.register(Choice)
