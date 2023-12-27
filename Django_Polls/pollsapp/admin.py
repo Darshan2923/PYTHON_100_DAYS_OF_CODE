cb6b10c4cd616b0a1dd0a7edbf0820fb5f4fb465
Darshan <darshanpatel2923@gmail.com>
2023-12-27 14:32:22 +0530

    By mistake

--- Django_Polls/pollsapp/admin.py
+++ Django_Polls/pollsapp/admin.py
@@ -1,18 +1,20 @@
 from django.contrib import admin
 from .models import *
 
-# class QuestionAdmin(admin.ModelAdmin):
-#     fields=['pub_date','question_text']
 
-# class QuestionAdmin(admin.ModelAdmin):
-#     fieldsets = [
-#         (None,               {'fields': ['question_text']}),
-#         ('Date information', {'fields': ['pub_date']}),
-#     ]
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields=['pub_date','question_text']
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
-    extra = 3
 
 
 class QuestionAdmin(admin.ModelAdmin):
@@ -36,6 +38,6 @@ class QuestionAdmin(admin.ModelAdmin):
 
 
 
-# Register your models here.
+# # Register your models here.
 admin.site.register(Question,QuestionAdmin)
-# admin.site.register(Choice)
+# # admin.site.register(Choice)
